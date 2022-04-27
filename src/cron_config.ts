2f5420b126fe9175a9afba739f12b29e31534b0c
Paul Millet <warstrolo.wizard@gmail.com>
2022-04-27 22:11:40 +0200

    fixed cron system

--- src/cron_config.ts
+++ src/cron_config.ts
@@ -2,6 +2,7 @@ import { config_type, crawl_urls_cron_config } from "./types";
 import puppeteer from "puppeteer"
 import Cron from "croner"
 import { writeFileSync, mkdirSync } from 'fs';
+import { Stack } from ".";
 
 
 /**
@@ -12,50 +13,20 @@ import { writeFileSync, mkdirSync } from 'fs';
  * @param config 
  * @param entry 
  */
-export function NewCronConfig(browser: puppeteer.Browser, config: config_type, entry: crawl_urls_cron_config) {
+export function NewCronConfig(entry: crawl_urls_cron_config, queue: Stack) {
   // log that cron was added
   console.log(`ADDED : ${entry.url} -> ${entry.file} | ${entry.cron}`)
 
-  const runner = async () => {
-    const page = await browser.newPage();
-    try {
-      // log cron start
-      console.log(`CAPTURE : ${entry.url} -> ${entry.file}`)
-
-      await page.setJavaScriptEnabled(true)
-
-      page.setDefaultTimeout(5000);
-      page.setDefaultNavigationTimeout(10000);
-
-      let url = (entry.url.includes("http")) ? entry.url : `${config.target}${entry.url}`
-      console.log(url)
-      await page.goto(url, {
-        waitUntil: ["domcontentloaded", "load", "networkidle2", "networkidle0"],
-      });
-      await page.waitForNetworkIdle()
-      await page.waitForTimeout(15000) // wait until react app is render (will change soon)
-      let ctn = await page.content()
-
-      console.log(`END CAPTURE : ${entry.url} -> ${entry.file}`)
-      const path = `${config.public}${entry.file.split("/").slice(0, -1).join("/")}`
-      mkdirSync(path, {
-        recursive: true
-      })
-      writeFileSync(`${config.public}/${entry.file}`, ctn)
-      
-      await page.close()
-    } catch (e) {
-      if (!page.isClosed()) {
-        await page.close()
-      }
-      console.log(e)
-    }
+  const runner = () => {
+    queue.push({
+      file: entry.file,
+      url: entry.url
+    })
   }
-  let crn = new Cron(entry.cron, async () => {
-    await runner()
+  new Cron(entry.cron, () => {
+    runner()
   });
 
-  (async () => {
-    await runner()
-  })()
+  runner()
+
 }
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@
 import handler from 'serve-handler';
 import http from 'http';
 import { readFileSync } from 'fs';
-import { config_type, crawl_urls_cron } from './types';
+import { config_type, crawl, crawl_urls_cron } from './types';
 import puppeteer from "puppeteer"
 import { NewCronConfig } from './cron_config';
 import { NewCronRemote } from './cron_remote';
-
-
+import Cron from 'croner';
+import { Crawl } from './crawl';
 
 
 const galaxiat_env = process.env.GALAXIAT_SERVE_ENV
@@ -20,32 +20,76 @@ console.log(`loading config ${config_location}`);
 
 const config: config_type = JSON.parse(readFileSync(config_location).toString());
 (async () => {
-  if (config["crawl"] != undefined) {
-    const browser = await puppeteer.launch({ headless: true, args: config.args });
-    for (const entry of (config["crawl"] as crawl_urls_cron[])) {
-      if (entry.type == "config") {
-        NewCronConfig(browser, config, entry)
-      } else if (entry.type == "remote") {
-        NewCronRemote(browser, config, entry)
-      } else {
-        console.log("ERROR : entry type not found")
+  const server = http.createServer((request, response) => {
+    return handler(request, response, {
+      public: config.public
+    });
+  })
+
+  const browser = await puppeteer.launch({ headless: false, args: config.args });
+  const borwserPID = browser.process();
+  let queue = new Stack()
+
+  let httpserv = server.listen(config.port, () => {
+    if (config["crawl"] != undefined) {
+
+      new Cron("*/15 * * * * *", async () => {
+        console.log(queue.count())
+        for (const entry of queue.get(10)) {
+          
+            await Crawl(browser, entry, config)
+          
+        }
+        console.log(browser.targets().length)
+      })
+      for (const entry of (config["crawl"] as crawl_urls_cron[])) {
+        if (entry.type == "config") {
+          NewCronConfig(entry, queue)
+        } else if (entry.type == "remote") {
+          NewCronRemote(entry, queue)
+        } else {
+          console.log("ERROR : entry type not found")
+        }
       }
+      //await browser.close()
+    } else {
+      console.log("No crawl params found")
     }
-    //await browser.close()
 
-  } else {
-    console.log("No crawl params found")
-  }
+    console.log(`Running at http://localhost:${config.port}`);
+  });
 
+  httpserv.on("close", () => {
+    browser.on('disconnected', () => {
+      console.log('sleeping 100ms'); //  sleep to eliminate race condition  
+      setTimeout(function () {
+        console.log(`Browser Disconnected... Process Id: ${process}`);
+        borwserPID?.kill()
+      }, 100);
+    })
+  })
 })();
 
-const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    public: config.public
-  });
-})
-
-
-server.listen(config.port, () => {
-  console.log(`Running at http://localhost:${config.port}`);
-});
+export class Stack {
+  list: crawl[]
+  constructor() {
+    this.list = []
+  }
+  public push(item: crawl): number {
+    console.log(item)
+    return this.list.indexOf(item) === -1 ? this.list.push(item) : this.list.length
+  }
+  public get(num: number): crawl[] {
+    let array : crawl[] = []
+    for (let i = 0; i < num; i++) {
+      let item = this.list.shift()
+      if (item) {
+        array.push(item)
+      }
+    }
+    return array;
+  }
+  public count(): number {
+    return this.list.length
+  }
+}

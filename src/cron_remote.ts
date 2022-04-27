8b5573a59262f0cb7d5ac63b2f1d5187ccecfc1b
Paul Millet <warstrolo.wizard@gmail.com>
2022-04-27 12:50:11 +0200

    fix page /2

--- src/cron_remote.ts
+++ src/cron_remote.ts
@@ -16,37 +16,40 @@ export function NewCronRemote(browser: puppeteer.Browser, config: config_type, e
   console.log(`ADDED : ${entry.json_url} -> REMOTE`)
 
   const runner = async () => {
-   
+
     try {
       console.log(`CAPTURE : ${entry.json_url} -> REMOTE`)
       let json: crawl_urls_cron_remote_json = await fetch(entry.json_url).then(res => res.json())
       for (const entry of json) {
         const page = await browser.newPage();
-        await page.setJavaScriptEnabled(true)
-        page.setDefaultTimeout(5000);
-        page.setDefaultNavigationTimeout(10000);
-        let url = (entry.url.includes("http")) ? entry.url : `${config.target}${entry.url}`
-        console.log(url)
-        await page.goto(url, {
-          waitUntil: ["domcontentloaded", "load", "networkidle2", "networkidle0"],
-        });
-        await page.waitForNetworkIdle()
-        await page.waitForTimeout(15000) // wait until react app is render (will change soon)
-        let ctn = await page.content()
+        try {
+          await page.setJavaScriptEnabled(true)
+          page.setDefaultTimeout(5000);
+          page.setDefaultNavigationTimeout(10000);
+          let url = (entry.url.includes("http")) ? entry.url : `${config.target}${entry.url}`
+          console.log(url)
+          await page.goto(url, {
+            waitUntil: ["domcontentloaded", "load", "networkidle2", "networkidle0"],
+          });
+          await page.waitForNetworkIdle()
+          await page.waitForTimeout(15000) // wait until react app is render (will change soon)
+          let ctn = await page.content()
 
-        console.log(`END CAPTURE : ${entry.url} -> ${entry.file}`)
-        const path = `${config.public}${entry.file.split("/").slice(0, -1).join("/")}`
-        mkdirSync(path, {
-          recursive: true
-        })
-        writeFileSync(`${config.public}/${entry.file}`, ctn)
+          console.log(`END CAPTURE : ${entry.url} -> ${entry.file}`)
+          const path = `${config.public}${entry.file.split("/").slice(0, -1).join("/")}`
+          mkdirSync(path, {
+            recursive: true
+          })
+          writeFileSync(`${config.public}/${entry.file}`, ctn)
 
-        await page.close()
+          await page.close()
+        } catch {
+          if (!page.isClosed()) {
+            page.close()
+          }
+        }
       }
     } catch (e) {
-      if (!page.isClosed()) {
-        page.close()
-      }
       console.log(e)
     }
   }
